7bc0ea72635d994455e2e756f7a7bcefcf581cc1
afgames <afgames@localhost.localdomain>
2026-08-09 20:29:21 +0300

    Set up CMake project

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "raylib.h"
+
+int main()
+{
+    const int screenWidth = 1000;
+    const int screenHeight = 1000;
+
+    InitWindow(screenWidth, screenHeight, "Added Difficulty");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
